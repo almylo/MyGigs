19c0241dab9db00be6815ba7619bb560dced660b
Alex Mylonas <almylo@Alexs-MacBook-Pro.local>
2014-09-13 20:59:27 +0300

    Initial Commit

---
+++ Sqlite Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sqlite Test
+//
+//  Created by Alex Mylonas on 9/13/14.
+//  Copyright (c) 2014 Alex Mylonas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Sqlite TestTests/Sqlite_TestTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sqlite_TestTests.swift
+//  Sqlite TestTests
+//
+//  Created by Alex Mylonas on 9/13/14.
+//  Copyright (c) 2014 Alex Mylonas. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Sqlite_TestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
